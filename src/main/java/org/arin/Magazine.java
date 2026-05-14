5ddd017d322cbbe5ed87bb2246ffa3b30b82a8c9
arin.kbr <arin.bornil@gmail.com>
2026-05-14 09:15:27 -0400

    add constructor

--- src/main/java/org/arin/Magazine.java
+++ src/main/java/org/arin/Magazine.java
@@ -4,4 +4,13 @@ public class Magazine extends Item {
 
     private int issueNumber;
     private String publisher;
+
+    public Magazine(int id, String title, String status,
+                    int issueNumber, String publisher) {
+
+        super(id, title, status);
+
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
