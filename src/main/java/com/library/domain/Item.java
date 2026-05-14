0280dbf63006d56cf085bdaaad06c0e595907eab
arin.kbr <arin.bornil@gmail.com>
2026-05-14 15:00:04 -0400

    add comparator and enum for item compare type

--- src/main/java/com/library/domain/Item.java
+++ src/main/java/com/library/domain/Item.java
@@ -2,6 +2,8 @@ package com.library.domain;
 
 import lombok.*;
 
+import java.util.Comparator;
+
 @Getter
 public abstract class Item {
     protected String title;
@@ -15,6 +17,32 @@ public abstract class Item {
         this.id = String.format("%04d", nextId++);
         this.title = title;
         this.status = status;
+    }
+
+    /**
+     * Converts item data into CSV format
+     * @return CSV formatted string containing item data
+     */
+    public abstract String toCSV();
+
+    @AllArgsConstructor
+    public static class itemComparator implements Comparator<Item> {
+        private ItemCompareType itemCompareType;
+
+        @Override
+        public int compare(Item o1, Item o2) {
+            return switch (itemCompareType) {
+                case TITLE -> o1.getTitle().compareToIgnoreCase(o2.getTitle());
+                case STATUS -> o1.getStatus().compareTo(o2.getStatus());
+                case ID -> o1.getId().compareTo(o2.getId());
+            };
+        }
+    }
+
+    public enum ItemCompareType {
+        TITLE,
+        STATUS,
+        ID
     }
 
     public enum Status {
