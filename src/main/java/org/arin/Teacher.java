13e85c17de57c438cc6cd4ce5dc6786f7901f10e
arin.kbr <arin.bornil@gmail.com>
2026-05-14 10:42:56 -0400

    Create teacher class

---
+++ src/main/java/org/arin/Teacher.java
@@ -0,0 +1,10 @@
+package org.arin;
+
+public class Teacher extends User {
+
+    private static final int MAX_ITEMS = 10;
+
+    public Teacher(int id, String name) {
+        super(id, name);
+    }
+}
