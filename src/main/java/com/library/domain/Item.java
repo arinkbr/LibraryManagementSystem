713a1fd17011d2302f0d07cd1f043f1a02f1e22d
arin.kbr <arin.bornil@gmail.com>
2026-05-14 14:58:41 -0400

    improve item class data and add enum status

--- src/main/java/com/library/domain/Item.java
+++ src/main/java/com/library/domain/Item.java
@@ -1,16 +1,25 @@
 package com.library.domain;
+
 import lombok.*;
 
 @Getter
-@Setter
-@ToString
-@AllArgsConstructor
 public abstract class Item {
+    protected String title;
+    @Setter
+    protected Status status;
+    protected String id;
 
-    private int id;
-    private String title;
-    private String status;
+    private static int nextId = 0;
 
-    public abstract String getDetails();
+    public Item(String title, Status status) {
+        this.id = String.format("%04d", nextId++);
+        this.title = title;
+        this.status = status;
+    }
 
+    public enum Status {
+        BORROWED,
+        INSTORE,
+        LOST
+    }
 }
