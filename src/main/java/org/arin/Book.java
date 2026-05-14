50f7e924208b365cb9a018e1b7c31fde57ae11c9
arin.kbr <arin.bornil@gmail.com>
2026-05-13 21:38:01 -0400

    Create Book class

---
+++ src/main/java/org/arin/Book.java
@@ -0,0 +1,25 @@
+package org.arin;
+
+public class Book extends Item {
+
+    private String isbn;
+    private String author;
+    private String genre;
+
+    public Book(int id, String title, String status, String isbn, String author, String genre) {
+        super(id, title, status);
+        this.isbn = isbn;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Book ID: " + getId() +
+                "\nTitle: " + getTitle() +
+                "\nStatus: " + getStatus() +
+                "\nISBN: " + isbn +
+                "\nAuthor: " + author +
+                "\nGenre: " + genre;
+    }
+}
