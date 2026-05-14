77ffa55cfc1343092f999eff21c4fdd287805fe8
arin.kbr <arin.bornil@gmail.com>
2026-05-14 09:58:56 -0400

    Create student class with constructor

---
+++ src/main/java/org/arin/Student.java
@@ -0,0 +1,10 @@
+package org.arin;
+
+public class Student extends User {
+
+    private static final int MAX_BOOKS = 5;
+
+    public Student(int id, String name) {
+        super(id, name);
+    }
+}
