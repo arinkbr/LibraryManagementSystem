db16ef42a48152f1be1ecc0f5c86f0a6c1756068
arin.kbr <arin.bornil@gmail.com>
2026-05-14 11:31:52 -0400

    create library class

---
+++ src/main/java/org/arin/Library.java
@@ -0,0 +1,16 @@
+package org.arin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+
+    private List<Item> items;
+    private List<User> users;
+
+    public Library() {
+        items = new ArrayList<>();
+        users = new ArrayList<>();
+    }
+}
