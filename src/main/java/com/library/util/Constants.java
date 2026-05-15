63991a3efac239c443759a09b7fbfa5b6b19d488
arin.kbr <arin.bornil@gmail.com>
2026-05-14 20:27:00 -0400

    create constants class

---
+++ src/main/java/com/library/util/Constants.java
@@ -0,0 +1,11 @@
+package com.library.util;
+
+public class Constants {
+
+    public static final int MAX_BOOKS_STUDENT = 5;
+    public static final int MAX_ITEMS_TEACHER = 10;
+    public static final String ITEMS_CSV_PATH = "src/main/resources/items.csv";
+    public static final String USERS_CSV_PATH = "src/main/resources/users.csv";
+    public static final String ITEMS_BACKUP_CSV_PATH = "src/main/resources/items_backup.csv";
+    public static final String USERS_BACKUP_CSV_PATH = "src/main/resources/users_backup.csv";
+}
